fd69c812a1c81d086ba9ab1a1a525072f48505a8
Anand Lunawat <aplunawat@gmail.com>
2023-07-31 00:39:42 +0530

    updated tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,15 +3,10 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
-    // using ./src/ dir
-    "./src/**/*.{js,ts,jsx,tsx}",
-    // using ./ dir
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    // add more paths here
   ],
   theme: {
     extend: {},
   },
   plugins: [],
+  purge: ["./pages/**/*.js", "./components/**/*.js"],
 }
