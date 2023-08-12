1c5612026fdc9332dbe5661bd9a50aa4c4f74205
Anand Lunawat <aplunawat@gmail.com>
2023-08-12 13:59:08 +0530

    Updated Loader

--- Components/Loader.jsx
+++ Components/Loader.jsx
@@ -2,10 +2,8 @@ import styles from "../styles/loader.module.css";
 
 export default function Loader() {
     return (
-        <div className="absolute inset-[40%]">
-            <div className={`${styles.loader} rounded-full shadow-[0px_0px_10px_2px] shadow-green-500 w-52 h-52 border-t-[10px] border-t-green-500`}>
-                <div className={`bg-black w-[220px] h-[220px] rounded-full`}></div>
-            </div>
+        <div className="absolute inset-[40%] max-sm:inset-[20%] max-sm:top-[30%]">            
+            <div className={`bg-black ${styles.loader} border-green-500 border-t-8 w-52 h-52 rounded-full`}></div>            
         </div>
     )
 }
