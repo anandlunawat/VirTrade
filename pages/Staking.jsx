a98f40ebe87411eda8cbf9625d0728ea5f44b9a9
Anand Lunawat <aplunawat@gmail.com>
2023-08-14 01:35:07 +0530

    Made SideDrawer Responsive

--- pages/Staking.jsx
+++ pages/Staking.jsx
@@ -107,11 +107,12 @@ export default function Staking() {
                             <span className="text-red-500">{NiftyBank}</span>
                         </button>
                     </div>
-                    <div className="md:basis-3/4 rounded-lg bg-opacity-40 bg-[#262424] w-full">
-                        <LineChart
+                    {/* <div className="md:basis-3/4 rounded-lg bg-opacity-40 bg-[#262424] w-full"> */}
+                        {/* <LineChart
                             chartN={chartN}
-                        />
-                    </div>
+                        /> */}
+                        <img src="/chart.png" className="p-2 rounded-lg h-52 md:basis-3/4" />
+                    {/* </div> */}
                 </div>
                 <CompanyCards
                     ltp={ltp}
